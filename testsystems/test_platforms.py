20e3667befa116e2a9da62ded750c499c13d131d
John Chodera (MSKCC) <choderaj@mskcc.org>
2013-11-10 11:28:02 -0500

    Added additional testing to break down discrepancies between platforms by Force object when platforms disagree.

--- testsystems/test_platforms.py
+++ testsystems/test_platforms.py
@@ -117,6 +117,56 @@ def compute_potential_and_force(system, positions, platform):
 
     return [potential, force]
 
+def compute_potential_and_force_by_force_index(system, positions, platform, force_index):
+    """
+    Compute the energy and force for the given system and positions in the designated platform for the given force index.
+
+    ARGUMENTS
+
+    system (simtk.openmm.System) - the system for which the energy is to be computed
+    positions (simtk.unit.Quantity of Nx3 numpy.array in units of distance) - positions for which energy and force are to be computed
+    platform (simtk.openmm.Platform) - platform object to be used to compute the energy and force
+    force_index (int) - index of force to be computed (all others ignored)
+
+    RETURNS
+
+    potential (simtk.unit.Quantity in energy/mole) - the potential
+    force (simtk.unit.Quantity of Nx3 numpy.array in units of energy/mole/distance) - the force
+
+    """
+
+    forces = [ system.getForce(index) for index in range(system.getNumForces()) ]
+    
+    # Get original force groups.
+    groups = [ force.getForceGroup() for force in forces ]
+    
+    # Set force groups so only specified force_index contributes.
+    for force in forces:
+        force.setForceGroup(1)
+    forces[force_index].setForceGroup(0) # bitmask of 1 should select only desired force
+
+    # Create a Context.
+    kB = units.BOLTZMANN_CONSTANT_kB
+    temperature = 298.0 * units.kelvin
+    kT = kB * temperature
+    beta = 1.0 / kT
+    collision_rate = 90.0 / units.picosecond
+    timestep = 1.0 * units.femtosecond    
+    integrator = openmm.LangevinIntegrator(temperature, collision_rate, timestep)
+    context = openmm.Context(system, integrator, platform)
+    # Set positions
+    context.setPositions(positions)
+    # Evaluate the potential energy.
+    state = context.getState(getEnergy=True, getForces=True, groups=1)
+    potential = state.getPotentialEnergy()
+    force = state.getForces(asNumpy=True)
+    
+    # Restore original force groups.
+    for index in range(system.getNumForces()):
+        forces[index].setForceGroup(groups[index])
+        
+    return [potential, force]
+
 #=============================================================================================
 # MAIN AND TESTS
 #=============================================================================================
@@ -155,6 +205,7 @@ if __name__ == "__main__":
         print "%s" % name
         for platform_index in range(openmm.Platform.getNumPlatforms()):
             
+            test_success = None
             try:
 
                 platform = openmm.Platform.getPlatform(platform_index)
@@ -191,24 +242,59 @@ if __name__ == "__main__":
                             for k in range(3):
                                 print "%12.6f" % (platform_force[atom_index,k]/force_unit),
                             print ""
-            
+
                 if test_success:
                     tests_passed += 1
                 else:
                     tests_failed += 1
 
-                    # Write XML files of failed tests to aid in debugging.
-                    print "Writing failed test system to '%s'.{system,state}.xml ..." % testsystem.name
-                    [system_xml, state_xml] = testsystem.serialize()
-                    xml_file = open(testsystem.name + '.system.xml', 'w')
-                    xml_file.write(system_xml)
-                    xml_file.close()
-                    xml_file = open(testsystem.name + '.state.xml', 'w')
-                    xml_file.write(state_xml)
-                    xml_file.close()
-                    
             except Exception as e:
                 print e
+
+            if (test_success is False):
+                # Write XML files of failed tests to aid in debugging.
+                print "Writing failed test system to '%s'.{system,state}.xml ..." % testsystem.name
+                [system_xml, state_xml] = testsystem.serialize()
+                xml_file = open(testsystem.name + '.system.xml', 'w')
+                xml_file.write(system_xml)
+                xml_file.close()
+                xml_file = open(testsystem.name + '.state.xml', 'w')
+                xml_file.write(state_xml)
+                xml_file.close()
+                
+                # Test by force group.
+                print ""
+                print "TESTING BY FORCE GROUP:"
+                nforces = system.getNumForces()
+                for force_index in range(nforces):
+                    force_name = system.getForce(force_index).__class__.__name__
+                    print force_name
+                    [reference_potential, reference_force] = compute_potential_and_force_by_force_index(system, positions, reference_platform, force_index)
+                    print "%32s %16s          %16s          %16s          %16s" % ("platform", "potential", "error", "force mag", "rms error")                        
+                    
+                    for platform_index in range(openmm.Platform.getNumPlatforms()):                            
+                        try:
+                            platform = openmm.Platform.getPlatform(platform_index)
+                            platform_name = platform.getName()
+                            [platform_potential, platform_force] = compute_potential_and_force_by_force_index(system, positions, platform, force_index)
+
+                            # Compute error in potential.
+                            potential_error = platform_potential - reference_potential
+
+                            # Compute per-atom RMS (magnitude) and RMS error in force.
+                            force_unit = units.kilocalories_per_mole / units.nanometers
+                            natoms = system.getNumParticles()
+                            force_mse = (((reference_force - platform_force) / force_unit)**2).sum() / natoms * force_unit**2
+                            force_rmse = units.sqrt(force_mse)
+                            
+                            force_ms = ((platform_force / force_unit)**2).sum() / natoms * force_unit**2
+                            force_rms = units.sqrt(force_ms)
+                                
+                            print "%32s %16.6f kcal/mol %16.6f kcal/mol %16.6f kcal/mol %16.6f kcal/mol" % (platform_name, platform_potential / units.kilocalories_per_mole, potential_error / units.kilocalories_per_mole, force_rms / force_unit, force_rmse / force_unit)
+                            
+                        except Exception as e:
+                            pass
+                print ""
 
     print "%d tests failed" % tests_failed
     print "%d tests passed" % tests_passed
